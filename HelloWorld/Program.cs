198299f532b1928c46e193cec857f2548131e946
Newt vo <nhutmvo1997@gmail.com>
2020-09-24 23:20:38 -0400

    Hello World in C#

---
+++ HelloWorld/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hom nay la thu " + 5);
+            Console.ReadLine();
+            
+        }
+    }
+}
