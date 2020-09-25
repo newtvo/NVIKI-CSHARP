f3a261047429d5302754d4b1149496cfae230813
Newt vo <nhutmvo1997@gmail.com>
2020-09-24 23:36:56 -0400

    Multiple namespace in C#

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -1,14 +1,40 @@
 ﻿using System;
 
+namespace FirstNameSpace
+{
+    public class Intro // khai bao class
+    {
+        public void sayHello() // Ham(method)
+        {
+            Console.WriteLine("I am in the first name space.");
+        }
+    }
+
+    namespace SecondNameSpace
+    {
+        public class Introduction
+        {
+            public void sayHello()
+            {
+                Console.WriteLine("I am in the second name space");
+            }
+        }
+    }
+}
+
 namespace HelloWorld
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hom nay la thu " + 5);
-            Console.ReadLine();
-            
+            // Khai bao doi tuong tu class
+            FirstNameSpace.Intro intro = new FirstNameSpace.Intro();
+            FirstNameSpace.SecondNameSpace.Introduction introduction = new FirstNameSpace.SecondNameSpace.Introduction();
+
+            // In ket qua ra console
+            intro.sayHello();
+            introduction.sayHello();
         }
     }
 }
